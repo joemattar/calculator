df05e308d47ccb7377a50e75ae1bf490e970896a
Joe M <joemattar@gmail.com>
2022-08-16 17:35:00 -0400

    Add equation parser function

--- js/javascript.js
+++ js/javascript.js
@@ -3,23 +3,6 @@ const lowerDisplay = document.querySelector(".display-lower");
 const buttonAc = document.querySelector("#ac");
 const buttonBackspace = document.querySelector("#backspace");
 
-const buttonPercent = document.querySelector("#percent");
-const buttonDivision = document.querySelector("#division");
-const buttonSeven = document.querySelector("#seven");
-const buttonEight = document.querySelector("#eight");
-const buttonNine = document.querySelector("#nine");
-const buttonMultiplication = document.querySelector("#multiplication");
-const buttonFour = document.querySelector("#four");
-const buttonFive = document.querySelector("#five");
-const buttonSix = document.querySelector("#six");
-const buttonSubstraction = document.querySelector("#substraction");
-const buttonOne = document.querySelector("#one");
-const buttonTwo = document.querySelector("#two");
-const buttonThree = document.querySelector("#three");
-const buttonAddition = document.querySelector("#addition");
-const buttonDecimal = document.querySelector("#decimal");
-const buttonZero = document.querySelector("#zero");
-const buttonTripleZero = document.querySelector("#triple-zero");
 const buttonEqual = document.querySelector("#equal");
 
 const allButtons = document.querySelectorAll("button");
@@ -33,6 +16,19 @@ function buttonEffect(button) {
     })
 }
 
+// Function to parse equation strings
+function stringParser(string) {
+    let equationArray = [];
+    string = string.trim();
+    
+    while (string.length !== 0) {
+        let token = string.match(/[0-9]+|[+]|[-]|[*]|[/]|[%]/);
+        string = string.replace(token[0], "");
+        array.push(token[0]);
+    }
+    console.log(equationArray);
+}
+
 // Event listerner for all buttons on click
 for (let button of allButtons) {
     button.addEventListener("click", () => {
@@ -40,8 +36,7 @@ for (let button of allButtons) {
         if (button.value) {
             lowerDisplay.textContent = lowerDisplay.textContent.concat(button.value);
         }
-    }
-    )    
+    })    
 }
 
 // Event listener for AC button on click
@@ -53,4 +48,9 @@ buttonAc.addEventListener("click", () => {
 // Event listener for backspace button on click
 buttonBackspace.addEventListener("click", () => {
     lowerDisplay.textContent = lowerDisplay.textContent.slice(0, lowerDisplay.textContent.length - 1);
+})
+
+// Event listener for equal button on click
+buttonEqual.addEventListener("click", () => {
+    stringParser(lowerDisplay.textContent);
 })
